cafbfcd1e65a1a27e666e05b857eb1333e3d3e83
CISSE <alyhabcis@gmail.com>
2022-08-10 10:55:03 +0200

    Ajout d'une barre de progression, puis modification de la taille, et des labels

--- PharmacieApp/Form1.Designer.cs
+++ PharmacieApp/Form1.Designer.cs
@@ -30,7 +30,48 @@ namespace PharmacieApp
         private void InitializeComponent()
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.guna2CircleProgressBar1 = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.labelPharmacie = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // guna2CircleProgressBar1
+            // 
+            this.guna2CircleProgressBar1.BackColor = System.Drawing.Color.Crimson;
+            this.guna2CircleProgressBar1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.guna2CircleProgressBar1.FillThickness = 10;
+            this.guna2CircleProgressBar1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.guna2CircleProgressBar1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleProgressBar1.Location = new System.Drawing.Point(311, 149);
+            this.guna2CircleProgressBar1.Minimum = 0;
+            this.guna2CircleProgressBar1.Name = "guna2CircleProgressBar1";
+            this.guna2CircleProgressBar1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleProgressBar1.Size = new System.Drawing.Size(170, 170);
+            this.guna2CircleProgressBar1.TabIndex = 0;
+            this.guna2CircleProgressBar1.Text = "guna2CircleProgressBar1";
+            // 
+            // labelPharmacie
+            // 
+            this.labelPharmacie.AutoSize = true;
+            this.labelPharmacie.Font = new System.Drawing.Font("MS PGothic", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPharmacie.ForeColor = System.Drawing.Color.White;
+            this.labelPharmacie.Location = new System.Drawing.Point(245, 54);
+            this.labelPharmacie.Name = "labelPharmacie";
+            this.labelPharmacie.Size = new System.Drawing.Size(311, 38);
+            this.labelPharmacie.TabIndex = 1;
+            this.labelPharmacie.Text = "Pharmacie CISSE";
+            this.labelPharmacie.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(223, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Developper par Aly";
             // 
             // Form1
             // 
@@ -38,17 +79,24 @@ namespace PharmacieApp
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Crimson;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelPharmacie);
+            this.Controls.Add(this.guna2CircleProgressBar1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Guna.UI2.WinForms.Guna2CircleProgressBar guna2CircleProgressBar1;
+        private System.Windows.Forms.Label labelPharmacie;
+        private System.Windows.Forms.Label label1;
     }
 }
 
